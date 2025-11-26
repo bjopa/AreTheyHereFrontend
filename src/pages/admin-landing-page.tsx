18d3762fcfc7062b8b8dddd8804ec9b548676e55
Björn Wange <bjopa78@gmail.com>
2025-11-26 10:07:04 +0100

    some colors changed, not correct yet

--- src/pages/admin-landing-page.tsx
+++ src/pages/admin-landing-page.tsx
@@ -1,15 +1,21 @@
-import React from "react";
-import AdminOptions from "../components/admin-options.tsx";
+import React from 'react'
+import AdminOptions from '../components/admin-options.tsx'
 
 const AdminLandingPage: React.FC = () => {
   return (
-    <>
+    <div
+      style={{
+        backgroundColor: 'var(--color-bg-primary)',
+        padding: '20px',
+        color: 'var(--color-fg-primary)',
+      }}
+    >
       <h1>ARE THEY HERE</h1>
       <h2>Admin Landing Page</h2>
       <p>Welcome to the admin landing page.</p>
       <AdminOptions />
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default AdminLandingPage;
+export default AdminLandingPage
