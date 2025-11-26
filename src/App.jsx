afae275abbcdb582673fc61b31cbde5727b52b8c
Björn Wange <bjopa78@gmail.com>
2025-11-26 09:51:51 +0100

    continued installation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import AdminPage from './pages/admin-landing-page.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <AdminPage />
+    </div>
+  )
+}
+
+export default App
